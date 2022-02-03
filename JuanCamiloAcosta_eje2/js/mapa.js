e25282539e1a35f2fb45fb9ac414b88df1015608
juacostar <juacostar@unal.edu.co>
2022-02-02 19:41:19 -0500

    parte 3 casi final

--- JuanCamiloAcosta_eje2/js/mapa.js
+++ JuanCamiloAcosta_eje2/js/mapa.js
@@ -1,7 +1,7 @@
 const resources = {
     "home":{
         "img_resource": "imagenes/Paipa_Boyaca.jpg" ,
-       "text":"<b>Home</b> <br> El mapa interactivo de Paipa es un resumen sobre la gran cantidad de lugares turísticos que contiene el municipio, dada us gran riqueza ambiental en cuanto a biodiversidad, histórica, agrónoma, entre otros lo que la hace ser la capital turística del departamento de Boyacá."
+       "text":"<b>Mapa interactivo de Paipa</b> <br> El mapa interactivo de Paipa es un resumen sobre la gran cantidad de lugares turísticos que contiene el municipio, dada us gran riqueza ambiental en cuanto a biodiversidad, histórica, agrónoma, entre otros lo que la hace ser la capital turística del departamento de Boyacá."
     },
     "Lago_Sochagota": {
        "img_resource": "imagenes/sochagota.jpg" ,
@@ -18,20 +18,35 @@ const resources = {
     "Palermo": {
         "img_resource": "imagenes/palermo.jpg" ,
         "text":"<b>Palermo</b> <br> El corregimiento de Palermo es un corregimiento a las afueras del municipio de Paipa, el cuál además de ser un atractivo turístico cuenta con cosechas de mora, agras, papa, entre otros y es un canal comercial entre los departamentos de Boyacá y Santander. Cuenta con un parque central y un recorrido para diversos eventos deportivos, dentro de los que se destaca el ciclismo."
+    },
+    "Pantano_Vargas": {
+        "url": "https://www.youtube.com/embed/AmkjKPUAsnE" ,
+        "text":"<b>Pantano de Vargas</b> <br> El pantano de Vargas es uno de los lugares históricos a nivel nacional que se encuentran en el municipio de Paipa. Localizado a las afueras del municipio, es un atractivo dado todos los lugares que contiene en donde se desató una de las batallas más importantes para la independencia de Colombia."
     }
 }
 
+var ac = 0;
+
+function resetVideo(){
+    var iframe = document.getElementById("info_video_map");
+    iframe.style.display = "none";
+    iframe.src = "none"
+}
 
 function displayImgText(index){
+    resetVideo();
     document.getElementById("img_resource").style.display = "block";
-    if(index == "Lago_Sochagota" || index == "Termales"){
-        document.getElementById("img_resource").style.width = "25%";
-    }
     document.getElementById("img_resource").src =  resources[index]["img_resource"];
     document.getElementById("text").innerHTML = resources[index]["text"];
     document.getElementById("info_map").scrollIntoView();
 }
 
 function displayVideoText(index){
-
+    var container = document.getElementById("info_map");
+    document.getElementById("img_resource").style.display = "none";
+    var iframe = document.getElementById("info_video_map");
+    iframe.style.display = "block";
+    iframe.src = resources[index]["url"];
+    document.getElementById("text").innerHTML = resources[index]["text"];
+    container.scrollIntoView();
 }
